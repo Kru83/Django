0f34bb6abb7996fbf44328badc972f54cb686873
Kru83 <kru1983@gmail.com>
2025-04-27 19:27:12 -0400

    added html zipcode input validation and limit of 5 digit.

--- PersonalWebSite/weather/tests.py
+++ PersonalWebSite/weather/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+import requests
 # Create your tests here.
+
+key = "Fu5mqpximlSEDp6LZOcCcVktlalCMe5wcHbAef6T"
+url = "https://api.congress.gov/v3/member?api_key="
+
+keyURL = url + key
+
+dataRequest = requests.get(keyURL)
+
+data = dataRequest.json()
+
+print(data)
+print(keyURL)
--- PersonalWebSite/weather/weather_nearby.py
+++ PersonalWebSite/weather/weather_nearby.py
@@ -11,26 +11,3 @@ def ipAddress():
     else:
         print(F"Error {ipAddressStatus}")
     return ipAddressData
-'''
-latitude = ipAddressData["geoplugin_latitude"]
-longitude = ipAddressData["geoplugin_longitude"]
-
-#calling the weatherData functions to store all weather data into dic from the API.
-weatherData = WeatherData(latitude, longitude,)
-
-#extract City and State from weatherData functions
-city = weatherData["properties"]["relativeLocation"]["properties"]["city"]
-state = weatherData["properties"]["relativeLocation"]["properties"]["state"]
-
-# This section uses the forecastRequest end point from free weather API to get actual forecast
-foreCastRequest = weatherData["properties"]["forecast"]
-
-foreCastData = ForeCastData(foreCastRequest)
-
-
-# temp and temp unit are captured from index values of 0, it's defined as This Afternoon, you can go index, 1, 2, and other for tonight and next day on and on
-temperature = foreCastData["properties"]["periods"][0]["temperature"]
-temperatureUnit = foreCastData["properties"]["periods"][0]["temperatureUnit"]
-detailedForecast = foreCastData["properties"]["periods"][0]["detailedForecast"]
-foreCastTime = foreCastData["properties"]["periods"][0]["name"]
-'''
